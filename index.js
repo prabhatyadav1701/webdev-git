04f8c23a2681ab37c4016408f0d48a44db268371
Prabhat Yadav <yadavprabhat79895@gmail.com>
2026-06-14 09:37:32 +0530

    Feat/multiply (#1)
    * new commit
    
    * new commit 2
    
    * hey i have added new commit

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ function sub(a,b){
     if(!(a>b)) throw new Error('num a must be gre then b')
     return a-b
 }
+
+function multiply(a,b){
+     return a*b
+}
+
 add(2,5)
 add(10,23)
 add(40,42)
+
+multiply(2,3)
+multiply(20,35)
+multiply(20,110)
+
